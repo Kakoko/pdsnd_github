178d60705f8c4b645feffddd9083e88ff4a09309
Simuzeche Kaluwa <Skaluwa@nitel.me>
2019-12-10 18:18:01 +0200

    Added additional Comments

--- bikeshare.py
+++ bikeshare.py
@@ -42,6 +42,8 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+		
+		Added More comments
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     print()
